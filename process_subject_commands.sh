553ef386bc1e7c092bb8837b4e1c97759323dc34
colmconn <colmconn@users.noreply.github.com>
2023-07-27 16:26:07 -0400

    Code to rerun preprecessing of functional tasks across all subjects and sessions.

--- process_subject_commands.sh
+++ process_subject_commands.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-if [[ 1 == 0 ]] ; then 
+##if [[ 1 == 0 ]] ; then 
 
-subject=176
-session=baseline
-echo ${subject}
-echo ${session}
-			    
-./00-make-new-subject-session-dirs.sh -s ${subject} -e ${session}
-./01-convertDicoms.sh -s ${subject} -e ${session}
+# subject=176
+# session=baseline
+# echo ${subject}
+# echo ${session}
 
-./02-run-freesurfer.sh -s ${subject} -e ${session} -h 4 -q
-./03-preprocess-anat-afni.sh -s ${subject} -e ${session} -h 4 -q
+# ./00-make-new-subject-session-dirs.sh -s ${subject} -e ${session}
+# ./01-convertDicoms.sh -s ${subject} -e ${session}
 
-## wait for the above commands to complete before running EPI procesing
-./04-preprocess-resting-state.sh --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
-./04-preprocess-task-tapping.sh --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
+# ./02-run-freesurfer.sh -s ${subject} -e ${session} -h 4 -q
+# ./03-preprocess-anat-afni.sh -s ${subject} -e ${session} -h 4 -q
+
+# ## wait for the above commands to complete before running EPI procesing
+# ./04-preprocess-resting-state.sh --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
+# ./04-preprocess-task-tapping.sh --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
 
 
-# subjects=$( cd ../sourcedata; ls -1d sub-1* )
+subjects=$( cd ../sourcedata; ls -1d sub-1* )
 
-# for subject in ${subjects} ; do
-#     for ses in baseline followup ; do
-# 	session=ses-${ses}
-# 	if [[ -d ../sourcedata/${subject}/${session} ]] ; then
-# 	    ./04-preprocess-resting-state.sh --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
-# 	    ./04-preprocess-task-tapping.sh  --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
-# 	fi
-#     done
-# done
+for subject in ${subjects} ; do
+    for ses in baseline followup ; do
+	session=ses-${ses}
+	if [[ -d ../sourcedata/${subject}/${session} ]] ; then
+	    ## ./04-preprocess-resting-state.sh --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
+	    ## ./04-preprocess-task-tapping.sh  --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
+	    ./04-preprocess-task-tapping-basis-tent.sh --subject ${subject} --session ${session} -h 4 -n -c 3 -b 4 -q
+	fi
+    done
+done
