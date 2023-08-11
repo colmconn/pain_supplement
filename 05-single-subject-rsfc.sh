12f339e9b0cd7380e3613398bcbfb98689b98e08
colmconn <colmconn@users.noreply.github.com>
2023-08-11 11:08:38 -0400

    Add scripts to perform single subject seed-based RSFC analysis and script to run all such analyses in parallel

---
+++ 05-single-subject-rsfc.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+# set -x
+
+#exit immediately on an error
+set -e 
+
+trap exit SIGHUP SIGINT SIGTERM
+
+. logger_functions.sh
+
+studyName=pain_supplement
+task="resting-state"
+
+PROGRAM_NAME=`basename $0`
+FULL_COMMAND_LINE="$( readlink -f $( dirname $0 ) )/$PROGRAM_NAME ${*}"
+
+GETOPT=$( which getopt )
+ROOT=${STUDY_ROOT:-/data/colmconn/$studyName}
+
+DATA=$ROOT/data
+SOURCE_DATA=$ROOT/sourcedata
+DERIVATIVE_DATA=$ROOT/derivative
+ANAT_PIPELINE_DIR=${DERIVATIVE_DATA}/afni-anat
+PREPROC_PIPELINE_DIR=${DERIVATIVE_DATA}/afni-${task}
+PIPELINE_DIR=${DERIVATIVE_DATA}/afni-${task}-corr
+LOG_DIR=$ROOT/log
+CODE_DIR=${ROOT}/code
+
+[[ ! -d ${LOG_DIR} ]] && mkdir -p ${LOG_DIR}
+
+#this is for the command line options; on mac would have to have gnu get opt installed; probably in macports, definitely in homebrew"
+GETOPT_OPTIONS=$( $GETOPT \
+		      -o "l:" \
+		      --longoptions "seedlist:,session:,subject:" \
+		      -n ${PROGRAM_NAME} -- "$@" )
+#exitStatus of getopt, getopt returns zero if had no parsing errors on
+#command line
+exitStatus=$?
+if [ $exitStatus != 0 ] ; then 
+    error_message_ln "Error with getopt. Terminating..." >&2 
+    exit $exitStatus
+fi
+
+#
+# Note the quotes around `$GETOPT_OPTIONS': they are essential!
+#
+# shift because command line arguments. shift command tells it to pop
+# them off the top so that don't get processed again.  shift 2 takes
+# off "-m .2"
+eval set -- "$GETOPT_OPTIONS"
+while true ; do 
+    case "$1" in
+	-l|--seedlist)
+	    seedList=$2; shift 2 ;;
+	--session)
+	    session=$2; shift 2 ;;
+	--subject)
+	    subject=$2; shift 2 ;;	
+	--) 
+	    shift ; break ;;
+	
+	*) 
+	    error_message_ln "${PROGRAM_NAME}: ${1}: invalid option" >&2
+	    exit 2 ;;
+    esac
+done
+
+if [ -z $subject ] ; then 
+    error_message_ln "ERROR: The subject ID was not provided."
+    exit
+fi
+
+if [ -z $session ] ; then 
+    error_message_ln "ERROR: The subject's session was not provided."
+    exit
+fi
+
+if [[ ${session:0:4} != "ses-" ]] ; then
+    session="ses-${session}"
+fi
+
+if [[ ${subject:0:4} != "sub-" ]] ; then
+    subject="sub-${subject}"
+fi
+
+if [ ! -f $seedList ] ; then
+    error_message "ERROR: The seed list file does not exit. Exiting"
+    exit
+else 
+    seeds=$( eval echo $( cat $seedList ) )
+fi
+
+info_message_ln "Computing RSFC for the following seeds:"
+info_message_ln "${seeds}"
+
+ss=${subject}/${session}
+
+preprocessedRsfcDir=${PREPROC_PIPELINE_DIR}/${ss}/${task}-preprocessed-polortA-NL
+
+if [[ -f ${preprocessedRsfcDir}/errts.${subject}_${session}_REML+tlrc.HEAD ]] ; then
+    if [[ ! -d ${PIPELINE_DIR}/subjects/${ss} ]] ; then
+	mkdir -p ${PIPELINE_DIR}/subjects/${ss}/
+    fi
+    
+    cd ${PIPELINE_DIR}/subjects/${ss}/
+    
+    for seed in ${seeds} ; do
+
+        seedName=${seed##*/}
+        if echo ${seedName} | grep -q "nii" ; then 
+            seedName=${seedName%%.nii*}
+        else 
+            seedName=${seedName%%+*}
+        fi
+
+        if [[ ! -d ${seedName} ]] ; then
+            mkdir ${seedName}
+        fi
+
+        info_message "Extracting timeseries for seed ${seed}"
+        3dROIstats -quiet -mask_f2short -mask ${seed} \
+		   ${preprocessedRsfcDir}/errts.${subject}_${session}_REML+tlrc.HEAD > ${seedName}/${seedName}.ts.1D
+
+        info_message "Computing Fisher Z-transformed Pearson Correlation for seed ${seedName}"
+        3dTcorr1D -pearson -Fisher \
+		  -mask ${preprocessedRsfcDir}/mask_group+tlrc.HEAD \
+		  -prefix ${seedName}/${seedName}.z-score \
+		  ${preprocessedRsfcDir}/errts.${subject}_${session}_REML+tlrc.HEAD \
+		  ${seedName}/${seedName}.ts.1D
+
+        3drefit -sublabel 0 ${subject}_${session} ${seedName}/${seedName}.z-score+tlrc.HEAD
+    done
+fi
---
+++ 06-run-all-rsfc-correlations.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# set -x
+
+#exit immediately on an error
+set -e 
+
+trap exit SIGHUP SIGINT SIGTERM
+
+. logger_functions.sh
+
+studyName=pain_supplement
+task="resting-state-seed-analysis"
+
+PROGRAM_NAME=`basename $0`
+FULL_COMMAND_LINE="$( readlink -f $( dirname $0 ) )/$PROGRAM_NAME ${*}"
+
+GETOPT=$( which getopt )
+ROOT=${STUDY_ROOT:-/data/colmconn/$studyName}
+
+DATA=$ROOT/data
+SOURCE_DATA=$ROOT/sourcedata
+DERIVATIVE_DATA=$ROOT/derivative
+LOG_DIR=$ROOT/log
+CODE_DIR=${ROOT}/code
+
+## the %P argument in printf prints the File's name with the name of
+## the starting-point under which it was found removed.
+## See https://man7.org/linux/man-pages/man1/find.1.html
+subjects=$( cd ${SOURCE_DATA}; find ./ -regex '.*sub-[0-9][0-9][0-9]/ses-[a-z]*' -printf "%P\n" )
+
+## echo ${subjects}
+
+taskFile=$CODE_DIR/run/${task}.taskfile
+info_message_ln "List of tasks to be executed is stored in $taskFile"
+
+cat /dev/null > $taskFile
+
+seedFiles="krienen-dlpfc-seeds.txt kelly-insula-seeds.txt goldapple-dlpfc-seeds.txt goldapple-dacc-mpfc-seeds.txt"
+for seeds in ${seedFiles} ; do
+    info_message_ln "Seed file: ${seeds}"
+    for ss in ${subjects} ; do
+	subject=${ss%%/*}
+	session=${ss##*/}
+	
+	echo "${CODE_DIR}/05-single-subject-rsfc.sh --subject ${subject} --session ${session} -l ${DERIVATIVE_DATA}/rsfc-seeds/${seeds}" >> ${taskFile} 
+    done
+done
+
+## jobname
+#$ -N $taskName
+
+## queue
+#$ -q all.q
+
+## binary? 
+#$ -b y
+
+## rerunnable?
+#$ -r y
+
+## merge stdout and stderr?
+#$ -j y
+
+## send no mail
+#$ -m n
+
+## execute from the current working directory
+#$ -cwd
+
+## use a shell to run the command
+#$ -shell yes 
+
+## set the shell
+#$ -S /bin/bash
+
+## preserve environment
+#$ -V 
+
+[[ ! -d $LOG_DIR ]] && mkdir $LOG_DIR
+queue=parallel.q
+nTasks=$( cat $taskFile | wc -l )
+sge_command="qsub -N ${task} -q ${queue} -j y -m n -V -wd $( pwd ) -o ${LOG_DIR} -t 1-${nTasks}" 
+echo $sge_command
+( exec $sge_command <<EOF
+#!/bin/sh
+
+#$ -S /bin/sh
+
+command=\`sed -n -e "\${SGE_TASK_ID}p" $taskFile\`
+
+exec /bin/sh -c "\$command"
+EOF
+)
+
+echo "Running qstat"
+qstat -q ${queue}
