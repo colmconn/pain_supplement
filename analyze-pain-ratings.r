f1831437c0235afce35c88d4a76db98acad78290
colmconn <colmconn@users.noreply.github.com>
2023-09-20 13:39:34 -0400

    Run statistics adn create graphs

--- analyze-pain-ratings.r
+++ analyze-pain-ratings.r
@@ -3,23 +3,13 @@
 rm(list=ls())
 graphics.off()
 
-library(tidyverse)
 library(RColorBrewer)
-## library(ggplot2)
+library(emmeans)
+library(ggplot2)
 library(magrittr)
-## library(ggforce)
-
-
-## from https://stackoverflow.com/questions/7705345/how-can-i-extract-plot-axes-ranges-for-a-ggplot2-object
-get_plot_limits <- function(plot) {
-    gb = ggplot_build(plot)
-    xmin = gb$layout$panel_params[[1]]$x.range[1]
-    xmax = gb$layout$panel_params[[1]]$x.range[2]
-    ymin = gb$layout$panel_params[[1]]$y.range[1]
-    ymax = gb$layout$panel_params[[1]]$y.range[2]
-    list(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)
-}
-
+library(nlme)
+library(tidyverse)
+library(cli)
 
 my.base.size=16
 my.theme=
@@ -44,32 +34,49 @@ my.theme=
 pain.ratings.file="../derivative/pain_ratings/pain_ratings.csv"
 pain.ratings = read_csv(pain.ratings.file,
                         col_types=cols(
-                            col_factor(), col_factor(), col_character(),
+                            col_character(), col_factor(), col_character(),
                             col_integer(), col_integer()),
                         col_names=c("subject", "session", "task", "run", "rating"))
+## the 6 in the filter comes from 2 sessions X 3 ratings (initial,
+## after tapping1 and after tapping 2)
+complete.pain.ratings=pain.ratings %>%
+    group_by(subject) %>%
+    summarize(nn=n()) %>%
+    filter(nn==6) %>%
+    pull(subject)
+## keep only subjects with complete data
+pain.ratings=pain.ratings %>%
+    filter(subject %in% complete.pain.ratings) %>%
+    arrange(subject, session)
 
 pd <- position_jitterdodge(jitter.height=0.1, dodge.width=0.1) # move them .1 to the left and right, and up or down
 colorCount=length(unique(pain.ratings$subject))
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 
-## geom_point(size=3, position=pd, shape=21, fill="white") %+%
 graph =
     pain.ratings %>%
-    mutate(task=paste(task, run)) %>% 
+    mutate(task=paste(task, run)) %>%
+    mutate(task = case_when(task == "initialpainrating 1" ~ "Initial",
+                            task == "kneeTapping 1"       ~ "Tapping 1",
+                            task == "kneeTapping 2"       ~ "Tapping 2")) %>%
     ggplot(aes(task, rating, color=subject, group=subject)) %+%
     geom_line(position=pd) %+%    
     geom_point(size=3, position=pd) %+%
-    scale_color_manual(name="Subject", values=getPalette(colorCount),
+    scale_color_manual(name="Subject",
+                       values=getPalette(colorCount),
                        aesthetics = c("color")) %+%
-    scale_x_discrete(labels=c("Initial", "Tapping 1", "Tapping 2")) %+%
     labs(title="Pain Ratings",
          x="Task",
          y="Rating") %+%
     facet_wrap(vars(session), labeller=as_labeller(str_to_title)) %+%
     my.theme
+##    scale_x_discrete(labels=c("Initial", "Tapping 1", "Tapping 2")) %+%
 
-dev.new(width=8, height=10, unit="in")  
-print(graph)
+## dev.new(width=8, height=10, unit="in")  
+## print(graph)
+graph.filename="../derivative/r-pain-ratings/pain_ratings.pdf"
+ggsave(graph.filename, graph, width=8, height=8, units="in")
+
 
 ## pr = pain ratings
 pr.wide=pain.ratings %>%
@@ -94,24 +101,105 @@ change.graph = pr.long %>%
     facet_wrap(vars(session), labeller=as_labeller(str_to_title)) %+%
     my.theme
 
-plot.limits=get_plot_limits(change.graph)
-## change.graph = change.graph %+%
-##     annotate("text", x=plot.limits$xmin-1.5, y=2,  label="Follow-up > Baseline", angle=90, hjust="left") %+%
-##     annotate("text", x=plot.limits$xmin-1.5, y=-1, label="Follow-up < Baseline", angle=90, hjust="left") %+%
-##     coord_cartesian(xlim=c(plot.limits$xmin, plot.limits$xmax), clip="off")
+## dev.new(width=8, height=10, unit="in")
+## print(change.graph)
+change.graph.filename="../derivative/r-pain-ratings/pain_ratings_change.pdf"
+ggsave(change.graph.filename, change.graph, width=8, height=8, units="in")
 
-dev.new(width=8, height=10, unit="in")
-
-## change.graph = change.graph %+%
-    
-print(change.graph)
-
-
-library (nlme)
+cli_h1("Baseline pain ratings model")
 
 baseline.pain.ratings=pain.ratings %>%
     filter(session=="baseline") %>%
     mutate(task=paste0(task, run)) %>%
     select(! run)
 
-baseline.model=lme(data=baseline.pain.ratings, rating ~ task, random=~1|subject)
+baseline.pain.ratings %>%
+    group_by(session, task) %>%
+    summarize(min=min(rating),
+              mean=mean(rating),
+              median=median(rating),
+              sd=sd(rating),
+              max=max(rating)) %>%
+    print(n=Inf)
+
+baseline.model=lme(data=baseline.pain.ratings,
+                   rating ~ task,
+                   random=~1|subject)
+cli_h2("LME Summary")
+print(summary(baseline.model))
+cli_h2("ANOVA Summary")
+print(anova(baseline.model))
+cli_h2("Pairwise comparisons")
+baseline.emm=emmeans(baseline.model, "task")
+print(contrast(baseline.emm, "pairwise"))
+
+cli_h1("Follow-up pain ratings model")
+followup.pain.ratings=pain.ratings %>%
+    filter(session=="followup") %>%
+    mutate(task=paste0(task, run)) %>%
+    select(! run)
+followup.pain.ratings %>%
+    group_by(session, task) %>%
+    summarize(min=min(rating),
+              mean=mean(rating),
+              median=median(rating),
+              sd=sd(rating),
+              max=max(rating)) %>%
+    print(n=Inf)
+
+followup.model=lme(data=followup.pain.ratings,
+                   rating ~ task,
+                   random=~1|subject)
+cli_h2("LME Summary")
+print(summary(followup.model))
+cli_h2("ANOVA Summary")
+print(anova(followup.model))
+cli_h2("Pairwise comparisons")
+followup.emm=emmeans(followup.model, "task")
+print(contrast(followup.emm, "pairwise"))
+
+
+cli_h1("Baseline to Follow-up mean pain ratings model") 
+mean.pain.ratings=pain.ratings %>%
+    mutate(task=paste(task, run)) %>%
+    select(!run) %>%
+    pivot_wider(names_from="task", values_from="rating") %>%
+    rowwise() %>%
+    mutate(mean.pain.rating=mean(c_across(!c(subject, session))))
+mean.pain.ratings %>%
+    group_by(session) %>%
+    summarize(min=min(mean.pain.rating),
+              mean=mean(mean.pain.rating),
+              median=median(mean.pain.rating),
+              sd=sd(mean.pain.rating),
+              max=max(mean.pain.rating)) %>%
+    print(n=Inf)
+
+pd <- position_jitterdodge(jitter.height=0.0, dodge.width=0.1) # move them .1 to the left and right, and up or down
+mean.pain.ratings.graph=mean.pain.ratings %>%
+    ggplot(aes(session, mean.pain.rating, color=subject, group=subject)) %+%
+    geom_line(position=pd) %+%    
+    geom_point(size=3, position=pd) %+%
+    scale_color_manual(name="Subject",
+                       values=getPalette(colorCount),
+                       aesthetics = c("color")) %+%
+    scale_x_discrete(labels=c("Baseline", "Follow up")) %+%
+    labs(title="Mean Pain Ratings",
+         x="Session",
+         y="Mean Pain Rating") %+%
+    my.theme
+## dev.new(width=8, height=10, unit="in")
+## print(mean.pain.ratings.graph)
+mean.pain.ratings.graph.filename="../derivative/r-pain-ratings/mean_pain_ratings.pdf"
+ggsave(mean.pain.ratings.graph.filename, mean.pain.ratings.graph, width=8, height=8, units="in")
+
+timepoint.model=lme(data=mean.pain.ratings,
+                    mean.pain.rating ~ session,
+                    random=~1|subject)
+cli_h2("LME Summary")
+print(summary(timepoint.model))
+cli_h2("ANOVA Summary")
+print(anova(timepoint.model))
+cli_h2("Pairwise comparisons")
+timepoint.emm=emmeans(timepoint.model, "session")
+print(contrast(timepoint.emm, "pairwise"))
