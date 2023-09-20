87867e6255935b5886230b974fdc215674e00643
colmconn <colmconn@users.noreply.github.com>
2023-09-20 12:08:32 -0400

    Use ther info.mesage functions instead oc cat int ehload and save functions

--- common.functions.r
+++ common.functions.r
@@ -31,30 +31,34 @@ info.message <- function(...) {
 }
 
 
-pigz.save <- function (..., list=character(), file=stop("'file' must be specified"), ncores=15) {    
+pigz.save <- function (..., list=character(), file=stop("'file' must be specified"), verbose=FALSE, ncores=15) {    
     zipper=suppressWarnings(system("which pigz", intern=TRUE, ignore.stderr=TRUE))
     if (length(zipper) > 0) {
         ## found pigz!
-        cat("*** Saving using pigz for compression\n")
+        if (verbose)
+            info.message("Saving using pigz for compression\n")
         con <- pipe(paste(zipper, "-p", ncores, ">", file), "wb")
         save(..., list=list, file = con)
         on.exit(close(con))
     } else {
-        cat("*** Saving using gzip for compression\n")
+        if (verbose)
+            info.message("Saving using gzip for compression\n")
         save (..., list=list, file=file)
     }
 }
 
-pigz.load <- function (file, envir = parent.frame(), verbose=FALSE, ncores=15) {    
+pigz.load <- function (file, envir = parent.frame(), verbose=FALSE, ncores=15) {
     zipper=suppressWarnings(system("which pigz", intern=TRUE, ignore.stderr=TRUE))
     if (length(zipper) > 0) {
         ## found pigz!
-        if (verbose) cat("*** Loading using pigz for decompression\n")
+        if (verbose)
+            info.message("Loading using pigz for decompression\n")
         con <- pipe(paste(zipper, "-dc", "-p", ncores, "<", file), "rb")
         load(file = con, envir=envir, verbose=verbose)
         on.exit(close(con))
     } else {
-        if (verbose) cat("*** Loading using gzip for decompression\n")
+        if (verbose)
+            info.message("Loading using gzip for decompression\n")
         load (file=file, envir=envir, verobse=verobse)
     }
 }
