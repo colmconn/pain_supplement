185eef03df11b06b9b5a19729ec6668d1ac2acc8
colmconn <colmconn@users.noreply.github.com>
2024-05-31 16:14:10 -0400

    Make BIDS compatible regressors

--- make-regressors.sh
+++ make-regressors.sh
@@ -33,6 +33,42 @@ blockDuration=$(( tappingDuration + restDuration ))
 nBlocks=6 # number of blocks of knee tapping + rest
 nRuns=2
 
+
+function make_bids_events_tsv {
+    local subject=$1
+    local session=$2
+    local task=$3
+    local run=$4
+    local afni_regressor=$5
+    
+    tsv_filename=${SOURCE_DATA}/${subject}/${session}/func/${subject}_${session}_task-${task}_run-${run}_events.tsv
+    json_filename=${SOURCE_DATA}/${subject}/${session}/func/${subject}_${session}_task-${task}_run-${run}_events.json
+    echo -e "onset\tduration\ttrial_type" > ${tsv_filename}
+    tail -n${run} ${afni_regressor} |
+	head -n1 |
+	tr ' ' '\n' |
+	sed -r '/^\s*$/d' |
+	awk -F':' -v tt=tapping 'BEGIN { OFS = "\t"; } {print $1, $2, tt}' >> ${tsv_filename}
+
+    cat <<EOF > ${json_filename}
+{
+    "trial_type": {
+        "LongName": "Tapping",
+        "Description": "When the knee was tapped during the experimental blocks",
+        "Levels": {
+            "tapping": "The knee is lightly tapped"
+        }
+    }
+}
+EOF
+
+}
+
+## the %P argument in printf prints the file's name with the name of
+## the starting-point under which it was found removed.
+## See https://man7.org/linux/man-pages/man1/find.1.html
+subjects=$( cd ${SOURCE_DATA}; find ./ -regex '.*sub-[0-9][0-9][0-9]/ses-[a-z]*' -printf "%P\n" )
+
 if [[ ! -d ${CODE_DIR}/regressors/ ]] ; then
     mkdir -p ${CODE_DIR}/regressors/
 fi
@@ -46,4 +82,12 @@ for rr in $( seq 1 ${nRuns} ) ; do
 	echo -n "${start}:${tappingDuration} " >> ${CODE_DIR}/regressors/task-tapping-married.1D
     done
     echo "" >> ${CODE_DIR}/regressors/task-tapping-married.1D
+     for ss in ${subjects} ; do
+	subject=${ss%%/*}
+	session=${ss##*/}
+	if [[ -f ${SOURCE_DATA}/${subject}/${session}/func/${subject}_${session}_task-tapping_run-${rr}_echo-1_bold.json ]] ; then 
+	    make_bids_events_tsv ${subject} ${session} "tapping" ${rr} ${CODE_DIR}/regressors/task-tapping-married.1D
+	fi
+     done
+    
 done
