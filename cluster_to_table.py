bb5e57bcc1809d961182df15e3f75f817c9a9f9c
colmconn <colmconn@users.noreply.github.com>
2025-04-30 12:46:37 -0400

    More command line argument checking.
    Print verbose messages to stderr.

--- cluster_to_table.py
+++ cluster_to_table.py
@@ -25,11 +25,15 @@ def setup_cmd_line_opts(args):
     return(options, args)
 
 def check_args(options, args):
+    if len(args) == 0:
+        print("No cluster location files provided on command line. Cannot continue", file=sys.stderr)
+        sys.exit(1)
+
     if options.verbose:
-        print(f"com   => {options.com}")
-        print(f"mi    => {options.mi}")    
+        print(f"com   => {options.com}", file=sys.stderr)
+        print(f"mi    => {options.mi}", file=sys.stderr)
         for ii, arg in enumerate(args):
-            print(f"{ii:02} -> {arg}")
+            print(f"{ii:02} -> {arg}", file=sys.stderr)
 
 
 def process_cluster_table(options, table_filename):
@@ -56,7 +60,7 @@ def process_cluster_table(options, table_filename):
     new_env['AFNI_WHEREAMI_NO_WARN']='y'
 
     result = subprocess.run(['whereami',
-                             "-space", options.space,
+                             "-space",  "MNI", # options.space,
                              "-coord_file", table_filename + '[' + columns + ']',
                              "-tab"],
                             env=new_env,
@@ -66,7 +70,7 @@ def process_cluster_table(options, table_filename):
     atlas_labels=[]
     if result.returncode == 0:
         if options.verbose:
-            print("Command executed successfully:")
+            print("Command executed successfully:", file=sys.stderr)
         
         result.stdout=result.stdout.removeprefix('++ Input coordinates orientation set by default rules to RAI\n').strip()
         cluster_count=0
@@ -95,11 +99,11 @@ def process_cluster_table(options, table_filename):
             cluster_count=cluster_count+1
 
         if options.verbose:          
-            print(f"Got {cluster_count:03d} atlas entries")
-            print(",".join(atlas_labels))
+            print(f"Got {cluster_count:03d} atlas entries", file=sys.stderr)
+            print(",".join(atlas_labels), file=sys.stderr)
             
         with open(names_filename, 'w') as ff:
-            print(f"{table_filename} -> {names_filename}")
+            print(f"Processing the clusters in {table_filename} -> {names_filename}")
             ff.write(",".join(atlas_labels) + '\n')
         
     else:
